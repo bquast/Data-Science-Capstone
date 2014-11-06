6e96d3a3a5b491076aec146730c7ca331fbe6aed
Bastiaan Quast <bquast@gmail.com>
2014-11-06 15:08:06 +0100

    change periods to underscores

--- Download.R
+++ Download.R
@@ -4,11 +4,11 @@
 #-------------------------
 
 # specify the source and destination of the download
-destination.file <- "Coursera-SwiftKey.zip"
-source.file <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+destination_file <- "Coursera-SwiftKey.zip"
+source_file <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
 
 # execute the download
-download.file(source.file, destination.file)
+download.file(source_file, destination_file)
 
 # extract the files from the zip file
-unzip(destination.file)
+unzip(destination_file)
