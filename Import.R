63b39911bd85f9d79dbb0d5b75d2e3a359233f72
Bastiaan Quast <bquast@gmail.com>
2014-11-06 15:08:20 +0100

    import the news file and save workspace image to file

--- Import.R
+++ Import.R
@@ -3,9 +3,18 @@
 # bquast@gmail.com
 # ------------------
 
-# import the data into the workspace
+# inspect the data
+list.files("final")
+US_files <-list.files("final/en_US")
+US_files
 
-dir("final")
+# import the data into the workspace
+blogs <- readLines("final/en_US/en_US.blogs.txt")
+twitter <- readLines("final/en_US/en_US.twitter.txt")
+
+con <- file("final/en_US/en_US.news.txt", open="rb")
+news <- readLines(con)
+close(con)
 
 # save the data to an .RData file
-
+save.image("en_US.RData")
