245143c7fb972fda2b62e1b4fa6fe42948b8b10e
Bastiaan Quast <bquast@gmail.com>
2014-11-11 16:41:57 +0100

    update with twitter emoticon cleaning

--- Import.R
+++ Import.R
@@ -8,13 +8,17 @@ list.files("final")
 US_files <-list.files("final/en_US")
 US_files
 
-# import the data into the workspace
-blogs <- readLines("final/en_US/en_US.blogs.txt")
-twitter <- readLines("final/en_US/en_US.twitter.txt")
+# import the blogs and twitter datasets in text mode
+blogs <- readLines("final/en_US/en_US.blogs.txt", encoding="UTF-8")
+twitter <- readLines("final/en_US/en_US.twitter.txt", encoding="UTF-8")
 
+# import the news dataset in binary mode
 con <- file("final/en_US/en_US.news.txt", open="rb")
-news <- readLines(con)
+news <- readLines(con, encoding="UTF-8")
 close(con)
+
+# clean the twitter dataset from non UTF-8 emoticons
+twitter <- iconv(twitter, from = "latin1", to = "UTF-8", sub="")
 
 # save the data to an .RData file
 save(blogs, news, twitter, file="en_US.RData")
