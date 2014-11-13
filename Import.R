7f2686c053bf37d36d4ba2f07a586ec20b83f13d
Bastiaan Quast <bquast@gmail.com>
2014-11-13 09:48:45 +0100

    update save method and ignoring new saved data

--- Import.R
+++ Import.R
@@ -5,8 +5,7 @@
 
 # inspect the data
 list.files("final")
-US_files <-list.files("final/en_US")
-US_files
+list.files("final/en_US")
 
 # import the blogs and twitter datasets in text mode
 blogs <- readLines("final/en_US/en_US.blogs.txt", encoding="UTF-8")
@@ -16,9 +15,12 @@ twitter <- readLines("final/en_US/en_US.twitter.txt", encoding="UTF-8")
 con <- file("final/en_US/en_US.news.txt", open="rb")
 news <- readLines(con, encoding="UTF-8")
 close(con)
+rm(con)
 
 # clean the twitter dataset from non UTF-8 emoticons
 twitter <- iconv(twitter, from = "latin1", to = "UTF-8", sub="")
 
-# save the data to an .RData file
-save(blogs, news, twitter, file="en_US.RData")
+# save the data to an .RData files
+save(blogs, file="blogs.RData")
+save(news, file="news.RData")
+save(twitter, file="twitter.RData")
