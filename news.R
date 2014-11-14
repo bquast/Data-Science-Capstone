906a1949c56ec3f3664ecc9c8dba3b5cc413aaae
Bastiaan Quast <bquast@gmail.com>
2014-11-14 11:25:33 +0100

    update bi and tri grams

--- news.R
+++ news.R
@@ -28,6 +28,13 @@ vc_news %>%
 tdm_news %>%
   findFreqTerms( lowfreq=100000 )
 
+# save tdm and free space
+tdm_news %>%
+  save( file = "tdm_news" )
+
+rm( tdm_news )
+gc()
+
 # n-grams
 
 # load the RWeka library
@@ -42,7 +49,7 @@ TriGramTokenizer <- function(x) NGramTokenizer(x,
                                                Weka_control(min = 3, max = 3)
                                                )
 
-# construct TDMs for bi and tri grams
+# construct TDMs for bigrams
 vc_news %>%
   TermDocumentMatrix(control = list(removePunctuation=TRUE,
                                     removeNumbers=TRUE,
@@ -51,8 +58,20 @@ vc_news %>%
                                     )
                      ) -> tdm_bi_news
 
-save( tdm_bi_news, file = "tdm_bi_news.RData")
+# list bi-grams with at least 500 occurances
+tdm_bi_news %>%
+  findFreqTerms( lowfreq=500 ) -> bi_news
 
+bi_news
+
+# save tdm bigrams and free space
+tdm_bi_news %>%
+  save( file = "tdm_bi_news.RData" )
+
+rm(tdm_bi_news)
+gc()
+
+# construct TDMs for bigrams
 vc_news %>%
   TermDocumentMatrix(control = list(removePunctuation=TRUE,
                                     removeNumbers=TRUE,
@@ -61,13 +80,16 @@ vc_news %>%
                                     )
                      ) -> tdm_tri_news
 
-save( tdm_tri_news, file = "tdm_tri_news.RData")
-
-# list bi-grams with at least 500 occurances
-tdm_bi_news %>%
-  findFreqTerms( lowfreq=500 ) -> bi_news
 
 # list tri-grams with at least 50 occurances
 tdm_tri_news %>%
   findFreqTerms( lowfreq=50 ) -> tri_news
 
+tri_news
+
+# save tdm bigrams and free space
+tdm_tri_news %>%
+  save( file = "tdm_tri_news.RData" )
+
+rm(tdm_tri_news)
+gc()
