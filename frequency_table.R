04a93d0635b5db0bf0648c0d8cf87c6b5de3d80a
Bastiaan Quast <bquast@gmail.com>
2014-11-21 10:29:22 +0100

    add bigram trigram tokaniser, create bigram TDM

--- frequency_table.R
+++ frequency_table.R
@@ -4,10 +4,19 @@
 
 # load the libraries
 library(tm)
+library(RWeka)
 library(dplyr)
 
+# ngram tokaniser
+min_words <- 2L
+max_words <- 2L
+bigram_token <- function(x) NGramTokenizer(x, Weka_control(min = min_words, max = max_words))
+min_words <- 3L
+max_words <- 3L
+trigram_token <- function(x) NGramTokenizer(x, Weka_control(min = min_words, max = max_words))
+
 # sample data
-zin <- "een lange nederlandse zin met niet al de veel de zelfde woorden maar wel ten minste een"
+zin <- "dit is een lange nederlandse zin met niet al de veel de zelfde woorden maar wel ten minste een paar"
 
 # frequency
 zin %>%
@@ -20,3 +29,7 @@ zin %>%
   VectorSource %>%
   VCorpus -> zin_corpus
 inspect(zin_corpus)
+
+# bigram Term-Document Matrix
+zin_corpus %>%
+  TermDocumentMatrix( control = list(tokenize = bigram_token) ) -> zin_tdm
