edefca069f258e1f4c5da7854b27ef72b80d2aa8
Bastiaan Quast <bquast@gmail.com>
2014-11-24 13:11:50 +0100

    fix wrong characters

--- Import.R
+++ Import.R
@@ -17,11 +17,6 @@ news <- readLines(con, encoding="UTF-8")
 close(con)
 rm(con)
 
-# clean the twitter dataset from non UTF-8 emoticons
-blogs <- iconv(blogs, from = "latin1", to = "UTF-8", sub="")
-news <- iconv(news, from = "latin1", to = "UTF-8", sub="")
-twitter <- iconv(twitter, from = "latin1", to = "UTF-8", sub="")
-
 # save the data to an .RData files
 save(blogs, file="blogs.RData")
 save(news, file="news.RData")
