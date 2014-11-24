72ad25e357596aa15723fb03d54c6922d40aa196
Bastiaan Quast <bquast@gmail.com>
2014-11-24 13:47:19 +0100

    cleanup and add string entering method

--- predict.R
+++ predict.R
@@ -3,6 +3,22 @@
 # bquast@gmail.com
 
 # load the libraries
-library(tm)
 library(e1071)
-library(klaR)
+
+# load the model
+load("model.RData")
+
+# create a test string
+test_string <- "accused of"
+
+# split it into separate words
+test_split <- strsplit(test_string, split = " " )
+
+# encode as a factor using the same levels
+test_factor <- factor(unlist(test_split), levels=news_levels)
+
+# transform to data frame
+test_df <- data.frame(X1 = test_factor[1], X2 = test_factor[2])
+
+# estimate using the model
+predict(tri.naiveBayes, test_df)
--- train_model.R
+++ train_model.R
@@ -3,26 +3,12 @@
 # bquast@gmail.com
 
 # load the libraries
-library(caret)
 library(e1071)
-library(klaR)
+library(dplyr)
 
 # e1071 package
-e1071.model <- naiveBayes( Y ~ X1 + X2 ,
-                           df_trigram )
+tri_naiveBayes <- naiveBayes( Y ~ X1 + X2 ,
+                              df_trigram )
 
-e1071.predictions <- predict(e1071.model, df_trigram[,-3])
-
-test_factor <- factor(c("accused", "of"), levels=news_levels)
-test_df <- data.frame(X1 =test_factor[1], X2 = test_factor[2])
-test_df
-predict(e1071.model, test_df)
-
-e1071.cf <- confusionMatrix( e1071.predictions,
-                             df_trigram[,3] )
-head(e1071.cf$table, 10)[,1:10]
-
-# klaR package
-klar.model <- NaiveBayes( df_trigram[,3] ~ ., df_trigram )
-
-klar.predictions <- predict(klar.model, grouping = test_factor )
+# save the model
+save(tri_naiveBayes, file = "model.RData")
